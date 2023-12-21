3e823b4ba80d85b91941b98f3c9018405826c340
Denis Chaput <denis.chaput77@gmail.com>
2023-12-21 17:18:21 +0100

    start test

---
+++ src/BowlingScore/BowlingGame.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BowlingScore
+{
+    public class BowlingGame
+    {
+        private int _pins1;
+        private int _pins2;
+        
+        public BowlingGame(int _pins1, int _pins2) { throw new NotImplementedException(); }
+        public void Roll_1(int _pins1) { }
+        public void Roll_2(int _pins2) { }
+        public int CalculateRoundScore() { throw new NotImplementedException();}
+    };
+}
+
