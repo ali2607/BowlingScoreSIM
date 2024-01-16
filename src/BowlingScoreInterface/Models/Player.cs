1902cb8dc5c77ef16739ff5292c8916e51b0dd8a
Elie Fares <ElieFares02@yahoo.com>
2024-01-16 12:09:25 +0100

    Implémentation de la logique de calcul de points par Player reste à faire: CalculateRoundScore

--- src/BowlingScoreInterface/Models/Player.cs
+++ src/BowlingScoreInterface/Models/Player.cs
@@ -2,7 +2,13 @@ namespace BowlingScoreInterface.Models;
 class Player
 {
     public string Name { get; private set; }
-    public List<(int Roll1, int Roll2)> Tab2DScores { get; private set; }
+    public enum SpecialRoll
+    {
+        Default = 0,
+        Spare=1,
+        Strike=2
+    }
+    public List<(int Roll1, int? Roll2, SpecialRoll specialRoll)> Tab2DScores { get; private set; }
     public Home Home { get; private set; }
     public int CurrentRound {  get; private set; } 
     int score_1, score_2;
@@ -12,39 +18,39 @@ class Player
         Name = name;
         Home = home;
         CurrentRound = -1;
-        Tab2DScores = [];    
-    }
-
-    public void Roll_1()
-    {
-        Console.WriteLine("Roll 1: ");
-        while (!int.TryParse(Console.ReadLine(), out score_1) || score_1 < 0 || score_1 > Home.NumberOfPins)
-        {
-            Console.WriteLine("Invalid input. Please enter a score between 0 and 10.");
-            Console.Write("Enter the score for the first roll: ");
-
-        }
-
+        Tab2DScores = new List<(int Roll1, int? Roll2, SpecialRoll specialRolll)>(Home.NumberOfRounds);
         
     }
-    public void Roll_2()
+
+    public void Roll1()
     {
+        CurrentRound++;
+        // asks the user to enter the result
         if (score_1 < Home.NumberOfPins)
+            Roll2();
+        else if (score_1 == Home.NumberOfPins)
         {
-            Console.WriteLine("Roll 2: ");
-            while (!int.TryParse(Console.ReadLine(), out score_2) || score_2 < 0 || score_2 + score_1 > Home.NumberOfPins)
-            {
-                Console.WriteLine("Invalid input. Please enter a score between 0 and 10.");
-                Console.Write("Enter the score for the second roll: ");
-            }
-
-
-
-            CurrentRound++;
+            Tab2DScores[CurrentRound] = (score_1, null, SpecialRoll.Strike);
+            CalculateRoundScore();
         }
+        else 
+            throw new Exception("Problem! Incorrect score!");
+    }
+    public void Roll2()
+    {
+        // asks the user to enter the result
+        if (score_1 + score_2 == Home.NumberOfPins)
+            Tab2DScores[CurrentRound] = (score_1, score_2, SpecialRoll.Spare);
+        else if(score_1 + score_2 < Home.NumberOfPins)
+            Tab2DScores[CurrentRound] = (score_1, score_2, SpecialRoll.Default);
+        else 
+            throw new Exception("Problem! Incorrect score!");
+
+        CalculateRoundScore();
     }
     public int CalculateRoundScore()
     {
+        // va regarder 1 ou 2 coups avant pour combler les potentiels trous avec les resultats récents
 
         throw new NotImplementedException();
     }
