f86bcb4a1740a905a20aadcb8a2b0da193b44cbb
Damache Kamil <kamildamache@gmail.com>
2024-01-17 12:19:31 +0100

    Ajout de la classe Round et ajouts dans la classe Player (calcul des scores)

---
+++ src/BowlingScoreInterface/Models/Round.cs
@@ -0,0 +1,19 @@
+﻿namespace BowlingScoreInterface.Models;
+
+public class Round
+{
+    public Player Player { get; set; }
+    public string FirstRound { get; set; }
+    public string SecondRound { get; set; }
+    public string RoundScore { get; set; } 
+
+    public Round (Player player, string firstRound, string secondRound, string roundScore)
+    {
+        Player = player;
+        FirstRound = firstRound;
+        SecondRound = secondRound;
+        RoundScore = roundScore;
+    }
+
+
+}
