0f94b2a3c85de824db69bdf5a0bd62f5024b1b17
Elie <eliefares02@yahoo.com>
2024-01-17 10:44:47 +0100

    [Backend] ScoreCalculator

--- src/BowlingScoreInterface/Models/Player.cs
+++ src/BowlingScoreInterface/Models/Player.cs
@@ -1,4 +1,5 @@
 namespace BowlingScoreInterface.Models;
+
 class Player
 {
     public string Name { get; private set; }
@@ -9,22 +10,26 @@ class Player
         Strike=2
     }
     public List<(int Roll1, int? Roll2, SpecialRoll specialRoll)> Tab2DScores { get; private set; }
+
     public Home Home { get; private set; }
     public int CurrentRound {  get; private set; } 
     int score_1, score_2;
-
+    public int TotalScore;
+    private bool displayScore;
+    private int waitingToDisplay = 2; //Roll remaining to display score
     public Player(string name, Home home)
     {
         Name = name;
         Home = home;
         CurrentRound = -1;
-        Tab2DScores = new List<(int Roll1, int? Roll2, SpecialRoll specialRolll)>(Home.NumberOfRounds);
+        Tab2DScores = new List<(int Roll1, int? Roll2, SpecialRoll specialRoll)>(Home.NumberOfRounds);
         
     }
 
     public void Roll1()
     {
         CurrentRound++;
+        waitingToDisplay --;
         // asks the user to enter the result
         if (score_1 < Home.NumberOfPins)
             Roll2();
@@ -38,11 +43,15 @@ class Player
     }
     public void Roll2()
     {
+        
         // asks the user to enter the result
         if (score_1 + score_2 == Home.NumberOfPins)
             Tab2DScores[CurrentRound] = (score_1, score_2, SpecialRoll.Spare);
         else if(score_1 + score_2 < Home.NumberOfPins)
+        {
             Tab2DScores[CurrentRound] = (score_1, score_2, SpecialRoll.Default);
+            waitingToDisplay--;
+        }
         else 
             throw new Exception("Problem! Incorrect score!");
 
@@ -50,7 +59,26 @@ class Player
     }
     public int CalculateRoundScore()
     {
-        // va regarder 1 ou 2 coups avant pour combler les potentiels trous avec les resultats récents
+        TotalScore += score_1 + score_2;
+
+        if (CurrentRound > 1)
+        {
+            if (Tab2DScores[CurrentRound - 1].SpecialRoll == SpecialRoll.Strike)
+            {
+                // Previous Roll was a strike
+                if (Tab2DScores[CurrentRound - 2].SpecialRoll == SpecialRoll.Strike)
+                {
+                    TotalScore += score_1;
+                }
+
+                TotalScore += score_1 + score_2;
+            }
+            else if (Tab2DScores[CurrentRound - 1].SpecialRoll == SpecialRoll.Spare)
+            {
+                // Previous Roll was a spare
+                TotalScore += score_1;
+            }
+        }
 
         throw new NotImplementedException();
     }
