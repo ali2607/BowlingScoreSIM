04b7863d84b2c2ca56a686911ad61b42c00d88d9
Ali <alilena999@gmail.com>
2024-01-21 13:24:45 +0100

    added the transition to the leaderboard + binded the restart game button

--- src/BowlingScoreInterface/Controllers/GameController.cs
+++ src/BowlingScoreInterface/Controllers/GameController.cs
@@ -41,6 +41,38 @@ namespace BowlingScoreInterface.Controllers
             //Mettre a jour le joueur actuel
             game = game.Update(pinsScore);
 
+            if(game.CurrentRound == game.NumberOfRounds)
+            {
+                string[] playerNames = new string[game.Players.Count];
+                int[] playerScores = new int[game.Players.Count];
+
+                for (int i = 0; i < game.Players.Count; i++)
+                {
+                    playerNames[i] = game.Players[i].Name;
+                    playerScores[i] = game.Players[i].TotalScore;
+                }
+
+                for (int i = 0; i < playerScores.Length; i++)
+                {
+                    for (int j = i + 1; j < playerScores.Length; j++)
+                    {
+                        if (playerScores[i] < playerScores[j])
+                        {
+                            // Échanger les scores
+                            int tempScore = playerScores[i];
+                            playerScores[i] = playerScores[j];
+                            playerScores[j] = tempScore;
+
+                            // Échanger les noms correspondants
+                            string tempName = playerNames[i];
+                            playerNames[i] = playerNames[j];
+                            playerNames[j] = tempName;
+                        }
+                    }
+                }
+                return RedirectToAction(nameof(Index), nameof(Leaderboard), new { names = playerNames, scores = playerScores });
+            }
+
             return View(nameof(Index), game);
         }
     }
